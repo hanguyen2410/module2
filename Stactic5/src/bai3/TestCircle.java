d30b8d40bfe8375a9f5e6f9a91857d72a90d9694
Ha Nguyen <nguyenvanha2410@gmail.com>
2022-08-12 17:30:47 +0700

    static

---
+++ Stactic5/src/bai3/TestCircle.java
@@ -0,0 +1,20 @@
+package bai3;
+
+public class TestCircle {
+    private double radius = 1.0;
+    private  String color = "red";
+     void Circle(){
+
+    }
+    void Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getArea() {
+        return color;
+    }
+}
---
+++ Stactic5/src/bai3/main.java
@@ -0,0 +1,9 @@
+package bai3;
+
+public class main {
+    public static void main(String[] args) {
+        TestCircle Circle = new TestCircle();
+        Circle.getRadius();
+        Circle.getArea();
+    }
+}
