e090ffdbabaf114f21c2fd442b286f17d8ae9c39
Ha Nguyen <nguyenvanha2410@gmail.com>
2022-08-12 17:13:06 +0700

    static

--- Class and OOP 4/src/bai4/main.java
+++ Class and OOP 4/src/bai4/main.java
@@ -7,7 +7,7 @@ public class main {
         Fan1.setOn(true);
         Fan1.setRadius(10);
         Fan1.setColor("yellow");
-        
+
         Fan Fan2 = new Fan();
         Fan2.setSpeed(2);
         Fan2.setOn(false);
