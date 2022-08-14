c5a80ff9722e6ba3707facfe3d9a0027bbd22221
Ha Nguyen <nguyenvanha2410@gmail.com>
2022-08-14 20:24:40 +0700

    static

---
+++ Stactic5/src/bai4/Student.java
@@ -0,0 +1,23 @@
+package bai4;
+
+public class Student {
+    String name;
+    String classes;
+    private String name() {
+       return name = "John";
+    }
+    private String classes(){
+        return classes = "CO2";
+    }
+    void Student(){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ Stactic5/src/bai4/main.java
@@ -0,0 +1,9 @@
+package bai4;
+
+public class main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Ha");
+        student.setClasses("H2O");
+    }
+}
